f022eadb1807d58a2df573bd217d855c0fc87992
PavanKalyan49r <pavan49r@gmail.com>
2024-01-31 14:51:32 +0530

    terraform

--- session01/ec2/ec2.tf
+++ session01/ec2/ec2.tf
@@ -1,32 +1,36 @@
 resource "aws_instance" "web" {
   ami           = "ami-0f3c7d07486cad139" #devops-practice
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.roboshop_all.id]
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
+
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloTerraform"
   }
 }
 
-resource "aws_security_group" "roboshop_all" { #this is terraform name
-  name        = "var.sg-name" #this is aws name
-  description = "var.sg-description"
-  #vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "roboshop-all" { #this is terraform name, for terraform reference only
+    name        = var.sg-name # this is for AWS
+    description = var.sg-description
+    #vpc_id      = aws_vpc.main.id
 
-  tags = {
-    Name = "roboshop_all_aws"
-  }
-}
+    ingress {
+        description      = "Allow All ports"
+        from_port        = var.inbound-from-port # 0 means all ports
+        to_port          = 0 
+        protocol         = "tcp"
+        cidr_blocks      = var.cidr_blocks
+        #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.roboshop_all.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = var.inbound-from-port # 0 mean all ports
-  ip_protocol       = "tcp"
-  to_port           = 0
-}
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        #ipv6_cidr_blocks = ["::/0"]
+    }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.roboshop_all.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+    tags = {
+        Name = "roboshop-all-aws"
+    }
 }
